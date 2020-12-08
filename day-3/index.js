1aaa5de1d4bb9a92ae387d936979e7e40df787c2
Alexander Botteram <a.botteram@gmail.com>
2020-12-08 12:29:47 +0100

    Finished day 3 assignment 2

--- day-3/index.js
+++ day-3/index.js
@@ -33,30 +33,37 @@ function convertVectorsToCoordinates( line ) {
         }
     } );
 
-    const coordinates = [ { x: 0, y: 0 } ];
+    const coordinates = [ { x: 0, y: 0, steps: 0 } ];
     for ( let i = 0; i < vectors.length; i++ ) {
         const { direction, length } = vectors[ i ];
         const lastCoordinate = coordinates[ i ];
+        const steps = lastCoordinate.steps + length;
         switch( direction ) {
             case "U":
-                coordinates.push( { x: lastCoordinate.x, y: lastCoordinate.y + length } ); break;
+                coordinates.push( { x: lastCoordinate.x, y: lastCoordinate.y + length, steps } ); break;
             case "D":
-                coordinates.push( { x: lastCoordinate.x, y: lastCoordinate.y - length } ); break;
+                coordinates.push( { x: lastCoordinate.x, y: lastCoordinate.y - length, steps } ); break;
             case "R":
-                coordinates.push( { x: lastCoordinate.x + length, y: lastCoordinate.y } ); break;
+                coordinates.push( { x: lastCoordinate.x + length, y: lastCoordinate.y, steps } ); break;
             case "L":
-                coordinates.push( { x: lastCoordinate.x - length, y: lastCoordinate.y } ); break;
+                coordinates.push( { x: lastCoordinate.x - length, y: lastCoordinate.y, steps } ); break;
         }
     }
 
     return coordinates;
 }
 
+function getManhattanDistance( p1, p2 ) {
+    return Math.abs( p1.x - p2.x ) + Math.abs( p1.y - p2.y );
+}
+
 ( async () => {
     const line1Coordinates = convertVectorsToCoordinates( ( await lineReader.next() ).value );
     const line2Coordinates = convertVectorsToCoordinates( ( await lineReader.next() ).value );
 
-    let smallest = Infinity;
+    let smallestManhattanDistance = Infinity;
+    let smallestStepsTaken = Infinity;
+
     for (let i = 0; i < line1Coordinates.length - 1; i++) {
         const line1 = new Line( {
             x1: line1Coordinates[ i ].x,
@@ -76,15 +83,25 @@ function convertVectorsToCoordinates( line ) {
             let intersection = line1.intersects( line2 );
 
             if ( intersection ) {
-                console.log( intersection );
-                const manhattan = Math.abs( intersection.x ) + Math.abs( intersection.y );
+                const manhattanDistance = getManhattanDistance( intersection, { x: 0, y: 0 } );
 
-                if ( manhattan < smallest ) {
-                    smallest = manhattan;
+                if ( manhattanDistance < smallestManhattanDistance ) {
+                    smallestManhattanDistance = manhattanDistance;
+                }
+
+                const stepsTaken = 
+                    line1Coordinates[ i ].steps +
+                    line2Coordinates[ j ].steps +
+                    getManhattanDistance( line1Coordinates[ i ], intersection ) +
+                    getManhattanDistance( line2Coordinates[ j ], intersection );
+
+                if ( stepsTaken < smallestStepsTaken ) {
+                    smallestStepsTaken = stepsTaken;
                 }
             }
         }
     }
 
-    console.log( smallest );
+    console.log( `Assignment 1: Intersection with the smallest manhattan distance to the source: ${ smallestManhattanDistance }` );
+    console.log( `Assignment 2: Intersection with the smallest amount of steps taken on line 1 and line 2 combined: ${ smallestStepsTaken }` );
 } )();
